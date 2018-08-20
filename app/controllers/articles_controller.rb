25ea3fd22aaa25b46a2abce37b3f501b1583a3f5
dschiumerini <dschiumerini@gmail.com>
2018-08-20 17:44:25 +0000

    Added styling to errors and messages partials.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,20 +16,20 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         
         if @article.save
-            flash[:notice] = 'Article was successfully created.'
+            flash[:success] = 'Article was successfully created.'
             redirect_to article_path(@article)
         else 
-            flash[:notice] = 'Could not create article.'
+            flash[:danger] = 'Could not create article.'
             render :new
         end
     end
     
     def update
         if @article.update(article_params)
-            flash[:notice] = 'Article was successfully updated.'
+            flash[:success] = 'Article was successfully updated.'
             redirect_to article_path(@article)
         else 
-            flash[:notice] = 'Could not update article.'
+            flash[:danger] = 'Could not update article.'
             render :edit
         end
     end
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = 'Aritcle was successfully deleted.'
+        flash[:danger] = 'Aritcle was successfully deleted.'
         redirect_to articles_path
     end
     
