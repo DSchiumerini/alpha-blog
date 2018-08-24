abd38235f7270350e7ffe81f992e7e09ff6ef3a1
dschiumerini <dschiumerini@gmail.com>
2018-08-24 15:11:30 +0000

    Created User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_154000) do
+ActiveRecord::Schema.define(version: 2018_08_24_145752) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_ad"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
